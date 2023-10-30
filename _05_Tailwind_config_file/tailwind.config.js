949a55274bb7a02f4465718203d9cd244da47532
danielOuattara <daniel.ouattara@gmx.com>
2023-10-30 16:53:38 +0100

    tailwind config file

---
+++ _05_Tailwind_config_file/tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/index.html", "./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontSize: {
+        elephant: "10rem",
+      },
+    },
+    colors: {
+      primary: "#ff6363",
+      secondary: {
+        100: "#e2e2d5",
+        200: "#888883",
+      },
+    },
+  },
+  plugins: [],
+};
