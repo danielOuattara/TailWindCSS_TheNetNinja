eb1b2672e1bedcd0766fded49146564a249a3d5b
danielOuattara <daniel.ouattara@gmx.com>
2023-11-02 19:37:15 +0100

    14 icons

---
+++ _14_Icons/tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/index.html", "./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontSize: {
+        elephant: "10rem",
+      },
+      fontFamily: {
+        nunito: ["Nunito"],
+      },
+      colors: {
+        primary: "#ff6363",
+        secondary: {
+          100: "#e2e2d5",
+          200: "#888883",
+          300: "#494946",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
